9683b01a71eb9e51d1fb40eb21586cccbcc85229
takao0206 <takao.haruna.0206@gmail.com>
2024-07-20 17:13:23 +0900

    bugカフェ プラクティス バク修正。
    1. ユーザインプットの数値とmenusの配列のインデックスが一致するように order_numberを修正。
    2. 注文の合計金額が文字列の足し算になっていたので、数値の足し算になるように修正。

--- cafe.rb
+++ cafe.rb
@@ -14,12 +14,20 @@ FOODS = [
   { name: 'ホットサンド', price: '410' }
 ].freeze
 
+def check_range(order_number, menus)
+  return unless order_number < 1 || order_number > menus.length
+
+  puts 'メニュー以外の番号が入力されました。はじめからやり直して下さい。'
+  exit
+end
+
 def take_order(menus)
   menus.each.with_index(1) do |menu, i|
     puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
   end
   print '>'
-  order_number = gets.to_i
+  order_number = gets.to_i - 1
+  check_range(order_number + 1, menus)
   puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
   order_number
 end
@@ -30,5 +38,5 @@ order1 = take_order(DRINKS)
 puts 'フードメニューはいかがですか?'
 order2 = take_order(FOODS)
 
-total = FOODS[order1][:price] + DRINKS[order2][:price]
+total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
